f38ce42336eeb4935a72212858388d9dc7edf34a
Dominic M <dominicccc@gmail.com>
2018-09-13 19:29:54 -0700

    Double click hides list items

--- app.js
+++ app.js
@@ -14,43 +14,47 @@ refreshPage();
 
   $("#add-text-btn").on("click", function(){
 
-    // store values from input boxes
+    // Store values from input boxes
     let inputKey = $("#user-input-title").val();
     let inputValue = $("#user-input-body").val();
 
-    // clear values from input boxes
+    // Clear values from input boxes
     $("#user-input-title").val("");
     $("#user-input-body").val("");
 
-    // add new property to local storage from input boxes
+    // Add new property to local storage from input boxes
     localStorage.setItem(inputKey, inputValue);
 
-    // add inputKey and inputValue to the #display container
+    // Add inputKey and inputValue to the #display container
     let itemHtml = '<li class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' - ' + '<span class="description">' + localStorage.getItem(inputKey) + '</span></li>';
     $(itemHtml).appendTo("#display");
 
   });
 
-  // Intention: when a display item is clicked, I want to add the class "checked" to it, so that styling is applied
+   // Apply styling to clicked list items
     $("#display").on("click", '*', function() {
       if (this.className === "display-item checked") {
         $(this).removeClass("checked");
       } else {
          $(this).addClass( "checked" );
        }
-       $("#user-input-title").val("");
-       $("#user-input-body").val("");
     });
 
+    // Hide a list item on double click
+    $("#display").on("dblclick", '*', function() {
+      $(this).hide();
+    });
+
+    // Delete a list item
     $("#del-text-btn").on("click", function() {
-      window.confirm('Task deleted!'); // maybe change to a window.confirm
-      localStorage.removeItem( $('#user-input-title').val() ); // grab the title and plop here
+      window.confirm('Task deleted!');
+      localStorage.removeItem( $('#user-input-title').val() );
       $("#user-input-title").val("");
       $("#user-input-body").val("");
       refreshPage();
     });
 
-    // clear local storage and refresh the #display container
+    // Clear local storage and refresh the #display container
     $("#clear-all-btn").on("click", function() {
       localStorage.clear();
       $("#user-input-title").val("");
