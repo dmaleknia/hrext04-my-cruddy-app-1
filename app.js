013e706b3c2007a9ac574a9251bb47328ae2f159
Dominic M <dominicccc@gmail.com>
2018-09-13 17:30:50 -0700

    Deletion seems to work

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 
   // Go through each property in localStorage to display the to do list on page refresh/start-up
+var refreshPage = function() {
+  $("#display").html("");
   for (var key in localStorage) {
     if (key !== "length" && key !== "key" && key !== "getItem" && key !== "setItem" && key !== "removeItem" && key !== "clear") {
-      let itemHtml = '<div class="display-item" data-storage-key="'+key+'"> ' + key + ' - ' + '<span class="description">' + localStorage.getItem(key) + '</span></div>';
+      let itemHtml = '<li class="display-item" data-storage-key="'+key+'"> ' + key + ' - ' + '<span class="description">' + localStorage.getItem(key) + '</span></li>';
       $(itemHtml).appendTo("#display");
     }
   }
+}
+refreshPage();
 
   $("#add-text-btn").on("click", function(){
 
@@ -22,16 +26,25 @@ $(document).ready(function() {
     localStorage.setItem(inputKey, inputValue);
 
     // add inputKey and inputValue to the #display container
-    let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' - ' + '<span class="description">' + localStorage.getItem(inputKey) + '</span></div>';
+    let itemHtml = '<li class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' - ' + '<span class="description">' + localStorage.getItem(inputKey) + '</span></li>';
     $(itemHtml).appendTo("#display");
 
   });
+
+   $(".display-item").on("click", function() {
+     window.confirm('Task deleted!'); // maybe change to a window.confirm
+     console.log(this);
+     localStorage.removeItem( $('#user-input-title').val() ); // grab the title and plop here
+     $("#user-input-title").val("");
+     $("#user-input-body").val("");
+   });
 
    $("#del-text-btn").on("click", function() {
      window.confirm('Task deleted!'); // maybe change to a window.confirm
      localStorage.removeItem( $('#user-input-title').val() ); // grab the title and plop here
      $("#user-input-title").val("");
      $("#user-input-body").val("");
+     refreshPage();
    });
 
    // clear local storage and refresh the #display container
@@ -44,26 +57,35 @@ $(document).ready(function() {
 
 });
 
-// Random heading and text colors
-function getColor() {
-  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-}
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-function randomizeSpans() {
-  $('span').each(function() {
-    $(this).css('color', getColor());
-  });
-}
 
-function randomizeHeadings() {
-  $('h1').each(function() {
-    $(this).css('color', getColor());
-  });
-}
+// // Random heading and text colors
+// function getColor() {
+//   return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+// }
 
-var span;
+// function randomizeSpans() {
+//   $('span').each(function() {
+//     $(this).css('color', getColor());
+//   });
+// }
 
-var colors = setInterval(function() {
-  span === 0 ? span = 1 : span = 0;
-  span === 0 ? randomizeSpans() : randomizeHeadings();
-}, 1000);
+// function randomizeHeadings() {
+//   $('h1').each(function() {
+//     $(this).css('color', getColor());
+//   });
+// }
+
+// var span;
+
+// var colors = setInterval(function() {
+//   span === 0 ? span = 1 : span = 0;
+//   span === 0 ? randomizeSpans() : randomizeHeadings();
+// }, 1000);
